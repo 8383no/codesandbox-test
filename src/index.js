e75d90d389a4e38d37e848505a28063db57c6b7e
8383no <83946745+8383no@users.noreply.github.com>
2021-09-16 22:30:23 +0900

    デフォルト値まで実行

--- src/index.js
+++ src/index.js
@@ -1,10 +1,118 @@
-import "./styles.css";
+/**
+ * const,let等の変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言不可能
+// let val2= "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // const変数は上書き不可
+// // val3 = "const変数を上書き";
+
+// const val3 = "const変数を再宣言";
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "hamino",
+//   age: 27
+// };
+// val4.name = "葉実";
+// val4.address = "Tokyo";
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "hamino";
+// const age = 27;
+// // 「私の名前はhaminoです。年齢は27歳です。」
+
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
+
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+// console.log(func1("func1です"));
+
+// // アロー関数
+// // reutunの一行のみで終わるような場合は、{}以降を下記のように省略できる
+// // const func2 = (str) => str;
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
+
+// // const func3 = (num1, num2) => num1 + num2;
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "hamino",
+//   age: 27
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myProfile = ["hamino", 27];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// // 配列から順番に取り出す
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+// 引数＝◯●で、引数に何も入っていない場合、◯●がデフォルト値として設定される
+// 引数＝◯●がない場合、undefinedとなってしまう。それを避けるために、上記のように書く
+const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+sayHello("hamino");
